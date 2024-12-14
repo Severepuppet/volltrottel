bf17f585d9c51f6b488fafa5f0bcd0aaeec42dc0
Severepuppet <153008432+Severepuppet@users.noreply.github.com>
2024-12-14 16:41:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 
 let stringWeihnachten = "Geschenkidee;Heißgetränk;beim Skifahren;im Ferienhaus;auf der Piste;unter dem Weihnachtsbaum;im Schnee;Weihnachtsessen;Weihnachtslied;Weihnachtsfilm;weihnachtlich;gut zum Berg runterfahren;ist weiß;macht man im Winter;Weihnachts-märchenfigur;Weihnachtsdeko;Winterliches Tier;Wintergetränk;Backzutat;wenns kalt ist;mit der Familie;Urlaubsziel; im Weihnachts-kalender;Weihnachtsduft";
-let stringClassic = "Farbe;Tier;kann man essen";
+let stringClassic = "Farbe;Tier;kann man essen;was Jeder braucht;Gewässer;dreht sich;Beruf;Männername;Grund zur Freude;kann sich öffnen;Pflanze;Obst;Eissorte;Gewürz;Frauenname;Sänger;Film;Politiker;Promi;Sportart;was Niemand braucht;Unwort;Serie;Hobby";
 let teile;
 let input;
+let stringAlles = stringWeihnachten +";"+ stringClassic;
 
 function getInputValue(){
     input = document.getElementById("input").value;
+    localStorage.setItem("LetzteURL",input);
 
     const url = new URL(window.location.href);
     url.searchParams.set("UserInput", input); // Parameter zur URL hinzufügen
@@ -25,8 +27,8 @@ function Urlabrufen(){
     const urlParams = new URLSearchParams(window.location.search);
     input = urlParams.get('UserInput');
     teile = input.split(";");
+    
     aktualisieren();
-
 }
 
 function weihnachten(){
@@ -36,8 +38,34 @@ function weihnachten(){
 
 function classic(){
     teile = stringClassic.split(";");
-    
     aktualisieren();
+}
+
+function Alles(){
+    teile = stringAlles.split(";");
+    aktualisieren(); 
+    teile = mischeArray(teile);
+    aktualisieren();
+
+}
+
+function letztes(){
+   teile = localStorage.getItem("LetzteURL").split(";");
+   aktualisieren();
+}
+
+function Farbe(){
+    let selected = document.querySelector('input[name="farbe"]:checked');
+    if (selected) {
+        let color = selected.value; // Den Wert (Farbe) holen
+        document.querySelectorAll(".orange").forEach(element => {
+            if(color == "blue"){element.style.background = "#0D98BA";}
+            if(color == "green"){element.style.background = "#63A355";}
+            if(color == "red"){element.style.background = "#ff4040";}
+            if (color == "pink") {element.style.background = "#FF69B4"}
+            if (color == "gray") {element.style.background = "#8e8e8e";}
+          });
+      }
 }
 
 function mischeArray(arr) {
@@ -46,6 +74,11 @@ function mischeArray(arr) {
         [arr[i], arr[j]] = [arr[j], arr[i]]; // Elemente tauschen
     }
     return arr;
+}
+
+function startSeite() {
+    const cleanURL = window.location.origin + window.location.pathname;
+  window.history.pushState({}, document.title, cleanURL);
 }
 
 function aktualisieren(){
@@ -78,6 +111,7 @@ document.getElementById("4spalte3").textContent = teile[20];
 document.getElementById("4spalte4").textContent = teile[21];
 document.getElementById("4spalte5").textContent = teile[22];
 document.getElementById("4spalte6").textContent = teile[23];
+Farbe();
 /*
 document.querySelectorAll(".orange").forEach(element => {
     element.style.background = "lightblue"; // Ändert die Schriftfarbe auf Blau
@@ -85,8 +119,5 @@ document.querySelectorAll(".orange").forEach(element => {
 window.print();*/
 }
 
-window.addEventListener("load", () => {
-    const url = new URL(window.location.href);
-    url.search = ""; // Alle Query-Parameter entfernen
-    window.history.replaceState({}, "", url);
-});
+
+
